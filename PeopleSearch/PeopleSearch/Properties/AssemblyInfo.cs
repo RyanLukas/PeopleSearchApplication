468cbe234ac03b55d60bb2340b418e8683392a1b
Ryan Lukas <ryanjlukas@outlook.com>
2020-03-25 16:16:52 -0600

    Changes

--- PeopleSearch/PeopleSearch/Properties/AssemblyInfo.cs
+++ PeopleSearch/PeopleSearch/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cde109ad-ee55-4524-9921-34c3fa291c53")]
+[assembly: Guid("88cd801e-16c4-4c3e-895a-cd051db8a65a")]
 
 // Version information for an assembly consists of the following four values:
 //
