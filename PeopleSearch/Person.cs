f4aec5a5a83b328ae66799838a4cc60d0eb69a32
Ryan Lukas <ryanjlukas@outlook.com>
2020-03-25 15:47:04 -0600

    Added Model of a person, and sqlserver inMemory

---
+++ PeopleSearch/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Person
+{
+    public string name { get; set; }
+    public string address{ get; set; }
+    public int age { get; set; }
+    public string interest { get; set; }
+    public Image picture { get; set; } 
+}
